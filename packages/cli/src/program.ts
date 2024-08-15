7cfeaeb1223eb9edde153762126061857f2a4407
microshine <microshine@mail.ru>
2024-08-15 16:08:58 +0200

    refactor: Remove getVersion function

--- packages/cli/src/program.ts
+++ packages/cli/src/program.ts
@@ -1,24 +1,5 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import { Command } from 'commander';
-
-/**
- * Get the version of the package
- * @returns The version of the package
- */
-export function getVersion(): string {
-  // Determine the base directory
-  const baseDir = __dirname.endsWith('src')
-    ? path.resolve(__dirname, '../') // Running from source when running via ts-node
-    : path.resolve(__dirname, '../../'); // Running from build when running via node
-  const packageJsonPath = path.join(baseDir, 'package.json');
-
-  // Read and parse the package.json file
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-  return packageJson.version;
-}
 
 export const program = new Command()
   .name('attest')
-  .description('Utility for verifying attestation data in various formats')
-  .version(getVersion());
+  .description('Utility for verifying attestation data in various formats');
