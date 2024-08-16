e7858c13a6e00af409e48985ef92458d080d9bb2
microshine <microshine@mail.ru>
2024-08-16 11:48:54 +0200

    test: Improve tests for validator

--- packages/marvell/src/validator.spec.ts
+++ packages/marvell/src/validator.spec.ts
@@ -1,6 +1,8 @@
 import assert from 'node:assert';
+import { AttestationVerificationFailure } from '@peculiar/attestation-common';
 import * as x509 from '@peculiar/x509';
 import { MarvellAttestationValidator } from './validator';
+import { OWNER_ROOT_CERT } from './certs';
 
 const base64CompressedData =
   'H4sIAAAAAAAAAGNgAANmBgYOBwYGdgUIl/0ZhFZaxsAIlONgngIVUwYqbYDIMTAyATGYwcwAYYFEGECYEchjZIExWGHSbDARdpgIB4zBBZNKhjHghiZBRUCOZGhILCiIz06tZBg4APZlQ4qBWXKyhYFhmnlKWlpKUkqKeaKlkYlBUkqyQWKqYWKKoZGJhZmRpYVBcqK5sVFyqqmJiVmaYXKKQZI5SG1iknGqsWFKkpGlqYVRokFSkoVJcpJhirlFiqF5khnQaLOkVDMj08RU41RzkxRjSzPLNMMU41QDC3NjC/Nk86QUoDsSge4ABbIjkF0MCp/GjcsZGARAwaTgULr46UI7Y84Qg2lae3adfm90vbjIpK299ZITx2RN+xNZQHUgv3BA/cTUwMBYAtJHwO//gepKiVRXRqS6ciLVVRCprpJIdQ3EqAMGLUiNI4uHUWDm4rJ2i9nf7um/Xi86I5Y5qKB7H5O29hyfvN8Vd+qv+vEqLF6Vx/YueCOrfqIUr9M2gUbP3u2dB6aH7WDkulUANEiRARxfjPOBdBsDJHUzCUAZjDwwRiqY0QDNSSAGPCukQBnAfImWF5mJyIsMsLzICMuLDLC8yAjLi4ywvMiAkRcZYXkRZM9oXsSeF7tG8yKyumGRF0Guw5EXGRtLNh6VepC1cUnAzPfnr3rYnfKe9/Dzyeb6y99ebEyz/XdD+WXTf/6G9m1P/wgq9nwRnbt1mu36kzMTS644NlXKR//pnSC7zMNKbhWDz7KJHNGbddqu8ksuuusdwyPmfzD9WMKXwq1Skx8a9jaeTec/Xte8eqftROmVG58+LQv73ainq7wzp/naxEdOYc/annt/jHryjO+M9puZXKn/lFNyJX7rLrkWUff2+BsZpltmDziT+E49UtRVOpqpy7Cve6qesGrgh+6P5xlaFz4v/p3d6ChbWr9z3XnZmgXXnuw8IpTELXA+4Wn7lgnSgf0/DKx7JofqTbc8U3mw/HnZSdc1GzZNf63s8l3lTrrG/wcAkcwjK0AIAAA=';
@@ -104,12 +106,23 @@ describe('Marvell:Validator', () => {
       return new x509.X509Certificate(cert);
     });
     const result = await validator.validate(data, certs);
-    //@ts-expect-error
-    assert.strictEqual(result.status, true, result.error?.message);
+    assert.strictEqual(
+      result.status,
+      true,
+      (result as AttestationVerificationFailure).error?.message,
+    );
     assert.strictEqual(
       result.signer.subjectName.getField('CN')[0],
       'HSM:5.3G1953-ICM001225:PARTN:1, for FIPS mode',
     );
-    assert.strictEqual(result.chain.length, 4);
+    assert.strictEqual(result.chain.length, 3);
+  });
+
+  it('should throw an error if initialized with a single trusted certificate', () => {
+    assert.throws(() => {
+      new MarvellAttestationValidator({
+        trustedCerts: [OWNER_ROOT_CERT],
+      });
+    });
   });
 });
